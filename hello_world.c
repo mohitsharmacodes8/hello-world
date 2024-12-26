1a71f369c08f3da034faa92f33f70c40334b784b
Mohit Sharma <mohitsharmacodes8@gmail.com>
2024-12-25 20:09:12 -0500

    Added user input for personalized greeting

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(){
+    char name[50];
     printf("Hello World!\n");
     printf("Welcome to Git!\n");
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s! Have a great day!\n", name);
     return 0;
 }
