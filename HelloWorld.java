674e6ed77d8835f4644c445e4ab7f7cb8c6c5541
Mohit Sharma <mohitsharmacodes8@gmail.com>
2024-12-25 23:20:29 -0500

    Version 3.2 - Fixed Scanner and corrected the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
+import java.util.Scanner; //import scanner class for user i/p
 public class HelloWorld{
     public static void main(String[] args){
-	System.out.println(args[0]+" "+args[1]);
+	Scanner sc = new Scanner(System.in);
+//Use next() to take a single word.
+//Or nextLine() to take a full line.
+//Or next().charAt(0) to take a single character.
+	String text;
+	System.out.println("Enter the text you wanna print");
+	text = sc.nextLine();
+	System.out.println(text);
+	sc.close(); //close Scanner
     }
 }
