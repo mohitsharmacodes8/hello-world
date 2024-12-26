3fd11b6bee4e87b539d7595a98f2cd768e26e344
Mohit Sharma <mohitsharmacodes8@gmail.com>
2024-12-25 23:12:21 -0500

    Version 3.1 - Error with Scanner and input issues

---
+++ HelloWorld_Error.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; //import scanner class for user i/p
+public class HelloWorld{
+    public static void main(String[] args){
+	Scanner sc = new Scanner();
+	String text;
+	text = sc.nextchar();
+	System.out.println(text);
+    }
+}
